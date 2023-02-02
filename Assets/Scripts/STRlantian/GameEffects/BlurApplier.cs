79dc215baa1513225d72a7798b5dbe56f2380d01
Strlantian <M1606977882@outlook.com>
2023-02-02 09:51:38 +0800

    2023.2.2 所以我要重写（（（

---
+++ Assets/Scripts/STRlantian/GameEffects/BlurApplier.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace STRlantian.GameEffects
+{
+    public class BlurApplier : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer sprRen;
+        [SerializeField]
+        private float speed = 0.5f;
+
+        private Material blur;
+        private int isEnable = 2;           //0 for yes, 1 for no, 2 for not active
+        private int itensID;
+
+        private void Start()
+        {
+            blur = sprRen.material;
+            itensID = Shader.PropertyToID("_Intensity");
+        }    
+
+        private void Update()
+        {
+            if(isEnable == 0)
+            {
+                ApplyBlur();
+            }
+            else if(isEnable == 1)
+            {
+                DisableBlur();
+            }
+        }
+
+        private async void ApplyBlur()
+        {
+            if(blur.GetFloat(itensID) < 20f)
+            {
+                blur.SetFloat(itensID, blur.GetFloat(itensID) + speed);     //Range: 0 - 20
+                await Task.Delay(1);
+            }
+            else
+            {
+                isEnable = 2;                                           //Idle state
+            }
+        }
+
+        private async void DisableBlur()
+        {
+            if(blur.GetFloat(itensID) > 0f)
+            {
+                blur.SetFloat(itensID, blur.GetFloat(itensID) - speed);
+                await Task.Delay(1);
+            }
+            else
+            {
+                isEnable = 2;
+            }
+        }
+
+        public void SetBlur(bool v)
+        {
+            if(v)
+            {
+                isEnable = 0;
+            }
+            else
+            {
+                isEnable = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/STRlantian/Util/STextRoller.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace STRlantian.Util
+{
+    public static class STextRoller
+    {
+        public static int wait
+        {
+            get { return wait; }
+            set
+            {
+                if (value > 0)
+                {
+                    wait = value;
+                }
+            }
+        }
+
+        public async static void RollText(Text field, string s)
+        {
+            if(!(wait > 0))
+            {
+                wait = 2;
+            }
+            char[] chars = s.ToCharArray();
+            foreach (char c in chars)
+            {
+                field.text += c;
+                await Task.Delay(wait);
+            }
+        }
+    }
+}
